4c79975cc5e4e7d4dc285983c791f78469d1988b
landresfernandezc <landresf3638@gmail.com>
2021-10-05 16:12:53 -0600

    Custom Hooks

---
+++ section15app-2/src/components/NewTask/NewTask.js
@@ -0,0 +1,31 @@
+import useHttp from '../../hooks/use-http';
+import Section from '../UI/Section';
+import TaskForm from './TaskForm';
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+  const createdTask=(taskText,taskData)=>{
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+      const createdTask = { id: generatedId, text: taskText };
+      props.onAddTask(createdTask);
+  }
+  const enterTaskHandler = async (taskText) => {
+    sendTaskRequest({
+      url: 'https://react-http-9bf01-default-rtdb.firebaseio.com/', 
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+      }, 
+      body: { text: taskText }
+    },createdTask.bind(null,taskText));
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
