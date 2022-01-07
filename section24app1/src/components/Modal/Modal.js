e1c6abc1f5d1ce7ab8f9bfc6444683e62a994bf6
Luis Fernandez <luiferna2@WKMZTA6E6A41.global.publicisgroupe.net>
2022-01-07 15:32:43 -0600

    Animating Lists with TransitionGroup

--- section24app1/src/components/Modal/Modal.js
+++ section24app1/src/components/Modal/Modal.js
@@ -13,7 +13,17 @@ const modal = (props) => {
       unmountOnExit
       in={props.show}
       timeout={animationTiming}
-      classNames="fade-slide"
+      classNames={
+        {
+          enter: '',
+          enterActive:'ModalOpen',
+          exit:'',
+          exitActive:'ModalClosed',
+          appear:'',
+          appearActive:'',
+
+        }
+      }
     >
       {(state) => {
         const cssClasses = [
